ca38e5d6577f2f5ba1446054cf02d6fbf8c42f09
yingweiy <yingweiy@gmail.com>
2016-04-05 20:13:49 -0500

    neck control

--- device/CameraServo.py
+++ device/CameraServo.py
@@ -4,8 +4,8 @@ class CameraServo:
     def __init__(self):
         self.pin_pan=18
         self.pin_tilt=19
-        self.pan_range=[80, 100]
-        self.tilt_range=[0,80]
+        self.pan_range=[60, 120]
+        self.tilt_range=[0, 80]
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin_pan, GPIO.OUT)
         GPIO.setup(self.pin_tilt, GPIO.OUT)
@@ -22,7 +22,7 @@ class CameraServo:
 
     def update_pan(self, angle):
         if angle<self.pan_range[0] or angle>self.pan_range[1]:
-            print('Error: out of pan angle boundary.', angle)
+            print('Error: out of pan angle boundary.', self.pan_degree)
             return
         self.pan_degree = angle
         self.pwm_pan.ChangeDutyCycle(self.ConvertDegree2DutyCycle(angle))
@@ -30,7 +30,7 @@ class CameraServo:
 
     def update_tilt(self, angle):
         if angle < self.tilt_range[0] or angle > self.tilt_range[1]:
-            print('Error: out of tilt angle boundary.', angle)
+            print('Error: out of tilt angle boundary.', self.tilt_degree)
             return
         self.tilt_degree = angle
         self.pwm_tilt.ChangeDutyCycle(self.ConvertDegree2DutyCycle(angle))
