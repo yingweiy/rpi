7a41caa49e91e7895c96d78c765fc363f4758eb4
yingweiy <yingweiy@gmail.com>
2016-04-21 16:39:59 -0500

    use netcat for camera streaming

--- device/IR.py
+++ device/IR.py
@@ -2,17 +2,20 @@ import RPi.GPIO as GPIO
 import time
 
 def init():
+    global IRPin
     IRPin = 13
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(IRPin, GPIO.IN)
 
 def loopCheck():
+    global IRPin
     while True:
         d = GPIO.input(IRPin)
         print(d)
         time.sleep(0.1)
 
 def OnHit():
+    global IRPin
     return GPIO.input(IRPin)
 
 
