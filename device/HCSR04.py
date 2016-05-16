cc8b0c50c6b4fdf34b7056ce17463c4a0031c3e1
Yingwei Yu <yingweiy@gmail.com>
2016-05-15 21:59:00 -0500

    updated the ultrasonic sensor

--- device/HCSR04.py
+++ device/HCSR04.py
@@ -16,21 +16,21 @@ class HCSR:
 
 
 	def measure(self):
-		GPIO.output(self.TRIG, True)
-		time.sleep(0.00001)
-		GPIO.output(self.TRIG, False)
+		distance = 0
+		for i in range(10):
+			GPIO.output(self.TRIG, True)
+			time.sleep(0.00001)
+			GPIO.output(self.TRIG, False)
 
-		while GPIO.input(self.ECHO)==0:
-			pulse_start = time.time()
+			while GPIO.input(self.ECHO)==0:
+				pulse_start = time.time()
 
-		while GPIO.input(self.ECHO)==1:
-			pulse_end = time.time()
+			while GPIO.input(self.ECHO)==1:
+				pulse_end = time.time()
 
-		pulse_duration = pulse_end - pulse_start
-		distance = pulse_duration * 17150
-		distance = round(distance, 2)
-		return distance
-
+			pulse_duration = pulse_end - pulse_start
+			distance += pulse_duration * 17150
+		return round(distance/10, 2)
 
 
 if __name__ ==  "__main__":
